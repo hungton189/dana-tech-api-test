a7f979a257662c66fc99ed14253c05b4cf64225b
hungton24hdev <hungton.24hdev@gmail.com>
2021-09-26 19:45:54 +0700

    init project

---
+++ app/main/routes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const glob = require("glob");
+const path = require("path");
+const _ = require("lodash");
+const routes = [];
+glob.sync("./**/routers.js").forEach((file) => {
+  console.log(file);
+  routes.push(require(path.resolve(file)));
+});
+module.exports = _.flattenDeep(routes);
